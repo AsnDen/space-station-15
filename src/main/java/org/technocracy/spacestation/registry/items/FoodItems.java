95a074d2661a02137da7ee34df9c5c61292be68f
AsnDen <asnden@gmail.com>
2026-08-22 21:47:52 +0300

    feat: tomato soup

--- src/main/java/org/technocracy/spacestation/registry/items/FoodItems.java
+++ src/main/java/org/technocracy/spacestation/registry/items/FoodItems.java
@@ -246,6 +246,12 @@ public final class FoodItems {
                     .recipeRemainder(Items.BUCKET))
     );
 
+    public static final Item SOUP_TOMATO_BLOOD = Registry.register(
+            Registries.ITEM, Identifier.of(SpaceStation.MOD_ID, "soup_tomato_blood"),
+            new Item(new Item.Settings().food(new FoodComponent.Builder()
+                            .nutrition(8).saturationModifier(2.5f).usingConvertsTo(BOWL_BIG).build()).maxCount(1))
+    );
+
     private FoodItems() {}
 
     public static void register() {}
