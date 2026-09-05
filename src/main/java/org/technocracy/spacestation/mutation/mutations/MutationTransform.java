5d513ebb2f1ca654ad60e206a719c9817d70b19f
AsnDen <asnden@gmail.com>
2026-09-05 17:39:02 +0300

    ref: move blockstate replacement to different class

--- src/main/java/org/technocracy/spacestation/mutation/mutations/MutationTransform.java
+++ src/main/java/org/technocracy/spacestation/mutation/mutations/MutationTransform.java
@@ -1,13 +1,12 @@
 package org.technocracy.spacestation.mutation.mutations;
 
 import net.minecraft.block.BlockState;
-import net.minecraft.state.property.Property;
-import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.random.Random;
-import net.minecraft.world.World;
 import org.technocracy.spacestation.mutation.Mutation;
 
 import java.util.List;
+
+import static org.technocracy.spacestation.mutation.BlockStateReplacer.replaceBlockState;
 
 public class MutationTransform extends Mutation {
 
@@ -31,34 +30,6 @@ public class MutationTransform extends Mutation {
         }
     }
 
-    private void setBlockState(BlockState newState, World world, BlockPos pos) {
-        BlockState oldState = world.getBlockState(pos);
-
-        for (Property<?> property : oldState.getProperties()) {
-            if (!newState.contains(property)) {
-                continue;
-            }
-
-            newState = copyProperty(oldState, newState, property);
-        }
-
-        world.setBlockState(pos, newState);
-    }
-
-    private static <T extends Comparable<T>> BlockState copyProperty(
-            BlockState oldState,
-            BlockState newState,
-            Property<T> property
-    ) {
-        T value = oldState.get(property);
-
-        if (property.getValues().contains(value)) {
-            return newState.with(property, value);
-        }
-
-        return newState;
-    }
-
     private BlockState chooseBlock(Random random) {
         double roll = random.nextDouble() * totalWeight;
 
@@ -77,7 +48,8 @@ public class MutationTransform extends Mutation {
     public boolean apply(MutationContext context) {
         Random random = context.world().getRandom();
         BlockState selected = chooseBlock(random);
-        setBlockState(selected, context.world(), context.pos());
+        BlockState newState = replaceBlockState(selected, context.world(), context.pos());
+        context.world().setBlockState(context.pos(), newState);
         return true;
     }
 
